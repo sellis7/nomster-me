64cc5365d272bc541acd648179bfb6c06c9a28fe
sellis7 <sellis7@users.noreply.github.com>
2015-11-14 16:48:38 -0500

    User comments now submittable to db

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Nomster::Application.routes.draw do
 
   devise_for :users
   root 'places#index'
+  resources :places do
+    resources :comments, :only => :create
+  end
 
   resources :places
   # The priority is based upon order of creation: first created -> highest priority.

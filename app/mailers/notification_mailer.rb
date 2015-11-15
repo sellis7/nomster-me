9b779e528d13fe086e494b7779fe90a2c1f3b672
sellis7 <sellis7@users.noreply.github.com>
2015-11-15 17:02:16 -0500

    Customization of ActionMailer/commented out true user experience for continued development of app

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,11 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added
+  def comment_added(comment)
+    @place = comment.place  # pulling from db
+    @place_owner = @place.user  # pulling specific owner of comment from db
     mail(to: 'webdev.throwaway@gmail.com',
-      subject: 'A comment was added to your posted location')
+      subject: "A comment was added to #{@place.name}")
+    ## Would be mail(to: @place_owner.email, ...) if owner of comment to receive msg
   end
 end

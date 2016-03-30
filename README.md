738dcbc0f1ed4d9adcde03bca54fe6cd09a95899
sellis7 <sellis7@users.noreply.github.com>
2016-03-30 15:31:13 -0400

    Seed.rb and README.md updated

---
+++ README.md
@@ -0,0 +1,27 @@
+## Nomster (or more preferably: "where it's @")
+
+A Yelp-type clone with Google Maps functionality and user features like comments, star ratings, image uploading, authentication, automated email notifications and dashboards.
+
+Though initially intended to promote food services, I opted for a means to locate and report "hot spots" in towns and cities where multiple, diverse shopping opportunities, restaurants, museums, theaters, and such can be sought out by travelers to the area. Also quite useful for new transplants to an area who could be looking for stuff to do, or just investigating their new surroundings.
+Hence – where it's @
+
+Tutorial-based build, but with self-directed tasks completed establishing the pagination, data validations and image upload functionality. Most of the application styling, and some structure, was personally customized.
+
+Technologies used and explored:
+* Ruby and Rails
+* PostgreSQL
+* Bootstrap
+* SCSS
+* Git, GitHub, and Heroku for the production platform
+* MVC design pattern
+* RESTful routing sans scaffolding
+* ActionMailer and Mailgun addon
+* Geocoder gem for geocoding & map integrations
+* Image uploading functionality by way of:
+  * CarrierWave gem
+	* MiniMagick gem
+	* AWS Amazon S3 & Fog gem
+* Kaminari gem for pagination
+* Devise gem for user authentication
+
+Check it out accessible via Heroku [here](https://sellis-nomster.herokuapp.com). Feel free to contribute your knowledge of great localities where there are unique and fun things to do and see! **You will not be emailed, as that feature was disabled within the app to avoid general spamming.** Additionally, an actual, valid email address is not required.
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,17 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Place.create!([
-  {name: "South Street", description: "A bit trendy now, but some remaining stores, restaurants, cafes, clubs & bars that still maintain a bit of the old bohemian/punk/alternative flavor that this locality used to be known for.", address: "South St from 33rd St to Front St, Philadelphia, PA"},
-  {name: "St. Mark's Place", description: "Somewhat edgy strip to roam encompassing alternative & varied vendors, storefronts, restaurants & bars. It was still quite the cool area to wander for unique & eclectic things back in 2008!", address: "8th St from Sixth Ave to Avenue D, New York, NY"},
-  {name: "Carytown", description: "Urban retail district of unique shops, variety of restaurants & assorted businesses originally established in the late '30s. Fun to wander, plus the Byrd Theatre is a great place to catch a cheap flick!", address: "Cary St from I195/Rt76 to Boulevard, Richmond, VA"},
-  {name: "Georgetown", description: "Eclectic blend of commercial, entertainment & residential district. TONS to see & quite the assortment of shops for all tastes... Commander Salamander - though now gone - was the retail staple of alternative culture in the area.", address: "Intersection of Wisconsin Ave & M St spanning about 0.5 mi in all directions, Washington, DC"},
-  {name: "Fell's Point", description: "Great way to spend an evening. Enormous selection of eateries & bars in addition to diverse retailers & services: get a tattoo, buy a surf board or expand your music collection all within a couple-block walk!", address: "S Broadway & Thames St intersection & roughly 3 block span, Baltimore, MD"},
+  {name: "South Street", description: "A bit trendy now, but some remaining stores, restaurants, cafes, clubs & bars that still maintain a bit of the old bohemian/punk/alternative flavor that this locality used to be known for.", address: "South St from 33rd St to Front St, Philadelphia, PA", user_id: nil, latitude: 39.941814, longitude: -75.149468},
+  {name: "St. Mark's Place", description: "Somewhat edgy strip to roam encompassing alternative & varied vendors, storefronts, restaurants & bars. It was still quite the cool area to wander for unique & eclectic things back in 2008!", address: "8th St from Sixth Ave to Avenue D, New York, NY", user_id: nil, latitude: 40.729386, longitude: -73.989518},
+  {name: "Carytown", description: "Urban retail district of unique shops, variety of restaurants & assorted businesses originally established in the late '30s. Fun to wander, plus the Byrd Theatre is a great place to catch a cheap flick!", address: "Cary St from I195/Rt76 to Boulevard, Richmond, VA", user_id: nil, latitude: 37.552856, longitude: -77.479922},
+  {name: "Georgetown", description: "Eclectic blend of commercial, entertainment & residential district. TONS to see & quite the assortment of shops for all tastes... Commander Salamander - though now gone - was the retail staple of alternative culture in the area.", address: "Intersection of Wisconsin Ave & M St spanning about 0.5 mi in all directions, Washington, DC", user_id: nil, latitude: 38.905217, longitude: -77.062842},
+  {name: "Fells Point", description: "Great way to spend an evening. Enormous selection of eateries & bars in addition to diverse retailers & services: get a tattoo, buy a surf board or expand your music collection all within a couple-block walk!", address: "S Broadway & Thames St intersection, Baltimore, MD (roughly 5 block span)", user_id: nil, latitude: 39.2839961, longitude: -76.5932996},
   {name: "Adamstown", description: "Adamstown, better-known as \"Antiques Capital U.S.A.\", is *the* place to go for antique/collectible enthusiasts. This stretch of Rt272 is lined with flea markets & antique shops & co-ops, so prepare to spend the day. Local eateries can be found to satisfy the appetite you'll get while hunting, too.", address: "N Reading Rd (Rt272), spanning Reamstown to Adamstown, PA"}
 ])
+Pic.create!([
+  {caption: "B-Boys & pie", place_id: 2, picture: "saintmarksPizza.jpg"}
+  ])
+Comment.create!([
+  {message: "Awesome spot!", rating: "5_stars", user_id: 2, place_id: 2},
+  {message: "Awful - worst place place I've ever been", rating: "1_star", user_id: 3, place_id: 5}
+  ])
